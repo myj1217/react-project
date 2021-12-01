5ba1acf23992562687716e1070f97fb65a290121
myj1217 <myj1217@naver.com>
2021-12-01 17:54:11 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="input_boxes">
+        <div className="input_box">
+          <div className="input_box_name">이름</div>
+          <input
+            type="text"
+            placeholder="이름"
+            name="name"
+            className="input_box_input"
+          />
+        </div>
+        <div className="input_box">
+          <div className="input_box_name">전화번호</div>
+          <input
+            type="text"
+            placeholder="전화번호"
+            name="phone"
+            className="input_box_input"
+          />
+        </div>
+        <button className="input_box_button">저장</button>
+      </div>
+
+      <div className="phone_list">
+        <div className="phone_item">
+          <div className="phone_item_left">
+            <div className="phone_item_name">홍길동</div>
+            <div className="phone_item_phone">010-0000-0000</div>
+          </div>
+          <div className="phone_item_right">
+            <button>삭제</button>
+          </div>
+        </div>
+        <div className="phone_item">
+          <div className="phone_item_left">
+            <div className="phone_item_name">홍길동</div>
+            <div className="phone_item_phone">010-0000-0000</div>
+          </div>
+          <div className="phone_item_right">
+            <button>삭제</button>
+          </div>
+        </div>
+        <div className="phone_item">
+          <div className="phone_item_left">
+            <div className="phone_item_name">홍길동</div>
+            <div className="phone_item_phone">010-0000-0000</div>
+          </div>
+          <div className="phone_item_right">
+            <button>삭제</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
