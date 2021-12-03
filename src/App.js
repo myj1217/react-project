039d23890fece8975ed7bc8a27bfe42dad341b50
myj1217 <myj1217@naver.com>
2021-12-03 19:47:27 +0900

    Create InputBox

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from "react";
+import InputBox from "./components/InputBox";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
-      <div className="input_boxes">
-        <div className="input_box">
-          <div className="input_box_name">이름</div>
-          <input
-            type="text"
-            placeholder="이름"
-            name="name"
-            className="input_box_input"
-          />
-        </div>
-        <div className="input_box">
-          <div className="input_box_name">전화번호</div>
-          <input
-            type="text"
-            placeholder="전화번호"
-            name="phone"
-            className="input_box_input"
-          />
-        </div>
-        <button className="input_box_button">저장</button>
-      </div>
-
+      <InputBox />
       <div className="phone_list">
         <div className="phone_item">
           <div className="phone_item_left">
