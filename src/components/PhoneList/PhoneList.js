cae2ac3079edf673116d12fa437dac8f194a7ed2
myj1217 <myj1217@naver.com>
2021-12-06 16:33:04 +0900

    Create PhoneItem

--- src/components/PhoneList/PhoneList.js
+++ src/components/PhoneList/PhoneList.js
@@ -1,36 +1,13 @@
 import React from "react";
 import "./PhoneList.css";
+import PhoneItem from "../PhoneItem";
 
 const PhoneList = () => {
   return (
     <div className="phone_list">
-      <div className="phone_item">
-        <div className="phone_item_left">
-          <div className="phone_item_name">홍길동</div>
-          <div className="phone_item_phone">010-0000-0000</div>
-        </div>
-        <div className="phone_item_right">
-          <button>삭제</button>
-        </div>
-      </div>
-      <div className="phone_item">
-        <div className="phone_item_left">
-          <div className="phone_item_name">홍길동</div>
-          <div className="phone_item_phone">010-0000-0000</div>
-        </div>
-        <div className="phone_item_right">
-          <button>삭제</button>
-        </div>
-      </div>
-      <div className="phone_item">
-        <div className="phone_item_left">
-          <div className="phone_item_name">홍길동</div>
-          <div className="phone_item_phone">010-0000-0000</div>
-        </div>
-        <div className="phone_item_right">
-          <button>삭제</button>
-        </div>
-      </div>
+      <PhoneItem />
+      <PhoneItem />
+      <PhoneItem />
     </div>
   );
 };
