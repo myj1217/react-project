e330eca83588a3555bb30c3a5fb8fee5843e39a9
myj1217 <myj1217@naver.com>
2021-12-07 19:05:25 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from "react";
-import InputBox from "./components/InputBox";
-import PhoneList from "./components/PhoneList";
-import "./App.css";
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="container">
-      <InputBox />
-      <PhoneList />
-    </div>
-  );
+class App extends Component {
+	state = { number: 0 }
+
+	// 1. state를 업데이트하는 함수를 전달
+	countUp = () => {
+		this.setState(
+			({ number }) => ({
+				number: number + 1
+			})
+		);
+	}
+
+	// 2. setState 함수 앞에서 값을 받고 이를 전달
+	countDown = () => {
+		const { number } = this.state
+		this.setState({ number: number - 1 });
+	}
+
+	render() {
+		// Destructuring Assignment 문법을 사용한 방식
+		const { number } = this.state;
+		const { countUp, countDown } = this;
+
+		return (
+			<div>
+				<div>
+					<button onClick={countUp}>+</button>
+					<button onClick={countDown}>-</button>
+				</div>
+				{this.state.number}
+			</div>
+		)
+	}
 }
 
 export default App;
