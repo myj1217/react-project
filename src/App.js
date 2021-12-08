a9e91c363afcc41822db97299614eb0b53c640a6
myj1217 <myj1217@naver.com>
2021-12-08 14:34:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import CountButton from "./components/CountButton";
+import Number from "./components/Number";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100px;
+  margin-top: 100px;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 50px;
+`;
 
 class App extends Component {
-	state = { number: 0 }
+  state = {
+    number: 0
+  };
 
-	// 1. state를 업데이트하는 함수를 전달
-	countUp = () => {
-		this.setState(
-			({ number }) => ({
-				number: number + 1
-			})
-		);
-	}
+  // 1. state를 업데이트하는 함수를 전달
+  countUp = () => {
+    this.setState(({ number }) => ({
+      number: number + 1
+    }));
+  };
 
-	// 2. setState 함수 앞에서 값을 받고 이를 전달
-	countDown = () => {
-		const { number } = this.state
-		this.setState({ number: number - 1 });
-	}
+  // 2. setState 함수 앞에서 값을 받고 이를 전달
+  countDown = () => {
+    const { number } = this.state;
+    this.setState({ number: number - 1 });
+  };
 
-	render() {
-		// Destructuring Assignment 문법을 사용한 방식
-		const { number } = this.state;
-		const { countUp, countDown } = this;
+  render() {
+    // Destructing Assignment 문법을 사용한 방식
+    const { countUp, countDown } = this;
+    const { number } = this.state;
 
-		return (
-			<div>
-				<div>
+    return (
+			<Wrapper>
+				<ButtonWrapper>
 					<button onClick={countUp}>+</button>
 					<button onClick={countDown}>-</button>
-				</div>
-				{this.state.number}
-			</div>
-		)
-	}
+				</ButtonWrapper>
+				{number}
+			</Wrapper>
+    );
+  }
 }
 
 export default App;
